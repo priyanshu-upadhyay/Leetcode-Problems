2ad7d5eb30b91039ef41d7b735e2de2262a7fdeb
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-04-12 19:06:05 +0530

    Time: 32 ms (79.86%), Space: 24.8 MB (99.43%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,7 +3,10 @@ public:
     void rotate(vector<int>& nums, int k) {
         k %=nums.size();
         reverse(nums.begin(), nums.end());
+        // 7 6 5 4 3 2 1
         reverse(nums.begin(), nums.begin()+k);
+        // 5 6 7 4 3 2 1
         reverse(nums.begin()+k, nums.end());
+        // 5 6 7 1 2 3 4
     }
 };
