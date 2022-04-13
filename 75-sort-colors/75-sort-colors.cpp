c91da60451ba89ab793b735597e4b5161416c5a1
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-04-13 20:21:00 +0530

    Time: 6 ms (16.71%), Space: 8.4 MB (24.37%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int red = 0, white = 0, blue = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 0) red++;
+            else if(nums[i] == 1) white++;
+            else blue++;
+        }
+        // Defining Start and End of Each Block;
+        int redStart = 0, redEnd = red;
+        int whiteStart = redEnd, whiteEnd = redEnd + white;
+        int blueStart = whiteEnd, blueEnd = whiteEnd + blue;
+        
+        // cout<<redStart<<" "<<redEnd<<"\n";
+        // cout<<whiteStart<<" "<<whiteEnd<<"\n";
+        // cout<<blueStart<<" "<<blueEnd<<"\n";
+        int i = 0;
+        
+        while(i < n)
+        {
+            if(nums[i] == 0 && redStart < redEnd)
+            {
+                swap(nums[i], nums[redStart++]);
+            }
+            else if(nums[i] == 1 && whiteStart < whiteEnd)
+            {
+                swap(nums[i], nums[whiteStart++]);
+            }
+            else if(nums[i] == 2 && blueStart < blueEnd)
+            {
+                swap(nums[i], nums[blueStart++]);
+            }
+            else
+            {
+                i++;
+            }
+        }
     }
 };
