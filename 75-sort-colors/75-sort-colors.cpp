eeec4f71e92ac29308ae2fea790e6c2f6b3b8344
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-04-13 20:58:17 +0530

    Time: 6 ms (16.71%), Space: 8.2 MB (70.46%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -9,34 +9,19 @@ public:
             else if(nums[i] == 1) white++;
             else blue++;
         }
-        // Defining Start and End of Each Block;
-        int redStart = 0, redEnd = red;
-        int whiteStart = redEnd, whiteEnd = redEnd + white;
-        int blueStart = whiteEnd, blueEnd = whiteEnd + blue;
         
-        // cout<<redStart<<" "<<redEnd<<"\n";
-        // cout<<whiteStart<<" "<<whiteEnd<<"\n";
-        // cout<<blueStart<<" "<<blueEnd<<"\n";
         int i = 0;
-        
-        while(i < n)
+        for(int j = 0; j < red; j++)
         {
-            if(nums[i] == 0 && redStart < redEnd)
-            {
-                swap(nums[i], nums[redStart++]);
-            }
-            else if(nums[i] == 1 && whiteStart < whiteEnd)
-            {
-                swap(nums[i], nums[whiteStart++]);
-            }
-            else if(nums[i] == 2 && blueStart < blueEnd)
-            {
-                swap(nums[i], nums[blueStart++]);
-            }
-            else
-            {
-                i++;
-            }
+            nums[i++] = 0;
+        }
+        for(int j = 0; j < white; j++)
+        {
+            nums[i++] = 1;
+        }
+        for(int j = 0; j < blue; j++)
+        {
+            nums[i++] = 2;
         }
     }
 };
