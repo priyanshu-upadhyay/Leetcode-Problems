27d0c9df0aa74e3509f4a5173df6ed43706bff10
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-04-22 13:53:35 +0530

    Time: 18 ms (56.79%), Space: 13.1 MB (82.52%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,39 +2,66 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& arr) {
         int n = arr.size(), m = arr[0].size();
-        vector<int> row(n), column(m);
+        
+        bool isRow = false, isCol = false;
         
         for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < m; j++)
+            if(arr[i][0] == 0) isRow = true;
+        }
+        
+        for(int j = 0; j < m; j++)
+        {
+            if(arr[0][j] == 0) isCol = true;
+        }
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
             {
                 if(arr[i][j] == 0)
                 {
-                    row[i] = 1;
-                    column[j] = 1;
+                    arr[0][j] = 0;
+                    arr[i][0] = 0;
                 }
             }
         }
         
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i < n; i++)
         {
-            if(row[i] == 1)
+            if(arr[i][0] == 0)
             {
-                for(int k = 0; k < m; k++)
+                for(int j = 0; j < m; j++)
                 {
-                    arr[i][k] = 0;
+                    arr[i][j] = 0;
                 }
             }
         }
         
-        for(int i = 0; i < m; i++)
+        for(int i = 1; i < m; i++)
         {
-            if(column[i] == 1)
+            if(arr[0][i] == 0)
             {
-                for(int k = 0; k < n; k++)
+                for(int j = 0; j < n; j++)
                 {
-                    arr[k][i] = 0;
+                    arr[j][i] = 0;
                 }
+            }
+        }
+        
+        if(isRow)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                arr[i][0] = 0;
+            }
+        }
+        
+        if(isCol)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                arr[0][j] = 0;
             }
         }
     }
