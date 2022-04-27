86d3c1aa48c870d02ff01be4dab30b631c7d3c5d
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-04-27 10:40:29 +0530

    Added solution - LeetHub

--- Killing Spree - GFG/killing-spree.cpp
+++ Killing Spree - GFG/killing-spree.cpp
@@ -6,16 +6,29 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 public:
+    long long int squareSum(long long int n)
+    {
+        return n*(n+1)*(2*n+1) / 6;
+    }
     long long int killinSpree(long long int n)
     {
-        long long int i = 1, c = 0;
-        while(n - i*i >= 0)
+        long long int start = 1, end = sqrt(n);
+        long long int ans = 1;
+        while(start <= end)
         {
-            c = c + 1;
-            n = n - i*i;
-            i++;
+            long long int mid = (start + end)/2;
+            long long int sum = squareSum(mid);
+            if(sum <= n)
+            {
+                ans = mid;
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
         }
-        return c;
+        return ans;
     }
 };
 
