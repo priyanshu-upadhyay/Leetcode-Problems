bd2c9d89db58ab47cfa1d277895daac6c050aee9
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-04-29 18:26:00 +0530

    Time: 108 ms (49.89%), Space: 69.4 MB (87.87%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
-        vector<int> effectiveLeftFuel(n);
-        for(int i = 0; i < n; i++)
-        {
-            effectiveLeftFuel[i] = gas[i] - cost[i];
-        }
+//         vector<int> effectiveLeftFuel(n);
+//         for(int i = 0; i < n; i++)
+//         {
+//             effectiveLeftFuel[i] = gas[i] - cost[i];
+//         }
         
-        int totalSum = accumulate(effectiveLeftFuel.begin(), effectiveLeftFuel.end(), 0);
-        if(totalSum < 0)
-        {
-            return -1;
-        }
-        int start = 0, currSum = 0;
+//         int totalSum = accumulate(effectiveLeftFuel.begin(), effectiveLeftFuel.end(), 0);
+        // if(totalSum < 0)
+        // {
+        //     return -1;
+        // }
+        int start = 0, currSum = 0, total = 0;
         for(int i = 0; i < n; i++)
         {
+            total += (gas[i] - cost[i]);
             if(currSum < 0)
             {
                 start = i;
                 currSum = 0;
             }
-            currSum += effectiveLeftFuel[i];
+            currSum += (gas[i] - cost[i]);
         }
-        return start;
+        return total < 0 ? -1 : start;
     }
 };
