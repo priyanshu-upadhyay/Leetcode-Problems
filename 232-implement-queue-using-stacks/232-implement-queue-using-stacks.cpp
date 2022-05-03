3de7459405c8d33fdc48e03b09af6c3eb4970aff
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-03 17:44:30 +0530

    Time: 2 ms (42.53%), Space: 7.1 MB (13.84%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -11,38 +11,25 @@ public:
     }
     
     int pop() {
-        while(!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        int front = s2.top();
+        int front = peek();
         s2.pop();
-        while(!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
         return front;
     }
     
     int peek() {
-        while(!s1.empty())
+        if(s2.empty())
         {
-            s2.push(s1.top());
-            s1.pop();
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
-        int front = s2.top();
-        while(!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
-        return front;
+        return s2.top();       
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
