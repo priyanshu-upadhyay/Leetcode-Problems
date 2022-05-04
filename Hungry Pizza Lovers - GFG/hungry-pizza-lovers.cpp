1102fa5e3ab379297c6048496781793e8662991e
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-04 10:34:20 +0530

    Added solution - LeetHub

---
+++ Hungry Pizza Lovers - GFG/hungry-pizza-lovers.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> permute(vector<vector<int>> &arr, int n);
+
+
+int main() {
+    
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            int a, b;
+            cin>> a>> b;
+            arr[i].push_back(a);
+            arr[i].push_back(b);
+        }
+        
+        vector<int> ans;
+        ans = permute(arr, n);
+        for(int i=0;i<n;i++)
+            cout << ans[i] << "\n";
+        
+    }
+    
+	return 0;
+}// } Driver Code Ends
+
+
+vector<int> permute(vector<vector<int>> &arr, int n)
+{
+    
+    vector<pair<int, int>> sum(n);
+    for(int i = 0; i < n; i++)
+    {
+        sum[i] = {arr[i][0] + arr[i][1], i + 1};
+    }
+    sort(sum.begin(), sum.end());
+    vector<int> result(n);
+    for(int i = 0; i < n; i++)
+    {
+        result[i] = sum[i].second;
+    }
+    return result;
+}
