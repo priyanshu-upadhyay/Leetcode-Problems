2a346d606d9bdfe7655c09bb8f9bb9901bd0dfae
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-09 00:07:28 +0530

    Time: 9 ms (5.59%), Space: 10.1 MB (7.03%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,29 +12,13 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        stack<TreeNode*> st;
-        while(!st.empty() || (p != NULL && q != NULL))
-        {
-            while(p != NULL && q != NULL)
-            {
-                st.push(p);
-                st.push(q);
-                p = p->left;
-                q = q->left;
-            }
-            
-            if(p != NULL || q != NULL) return false;
-            TreeNode* qTop = st.top();
-            st.pop();
-            TreeNode* pTop = st.top();
-            st.pop();
-            // cout<<qTop->val<<" "<<pTop->val<<"\n";
-            if(qTop->val != pTop->val) return false;
-            q = qTop->right;
-            p = pTop->right;
-        }
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
         
-        if(p != NULL || q != NULL) return false;
-        return true;
+        if(p->val == q->val)
+        {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        return false;
     }
 };
