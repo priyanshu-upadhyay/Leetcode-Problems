ccdeb88412e1d4b8c99a64a7b5c7c7ed8397b195
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-12 14:38:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (16.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
-private:
-    int dp[46];
-    int climbHelper(int n)
-    {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = climbHelper(n - 1) + climbHelper(n - 2);
-    }
 public:
     int climbStairs(int n) {
-        memset(dp, -1, sizeof(dp));
-        return climbHelper(n);
+        vector<int> dp(n + 2, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        int sum = 0;
+        for(int i = 2; i <= n + 1; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n + 1];
+        
     }
 };
