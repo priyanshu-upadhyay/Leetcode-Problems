92292915af2a496f981f09c52b25dc033cb26184
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-12 12:21:18 +0530

    Time: 80 ms (19.72%), Space: 9.2 MB (56.98%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 private:
-    void genrateCombinations(vector<int>& nums, vector<int>& temp, set<vector<int>>& result)
+    void genrateCombinations(vector<int>& nums, int c, set<vector<int>>& result)
     {
-        if(nums.size() == 0)
+        if(c == nums.size())
         {
-            result.insert(temp);
+            result.insert(nums);
             return;
         }
         int n = nums.size();
-        for(int i = 0; i < n; i++)
+        for(int i = c; i < n; i++)
         {
-            temp.push_back(nums[i]);
-            vector<int> new_nums;
-            for(int j = 0; j < i; j++) new_nums.push_back(nums[j]);
-            for(int j = i + 1; j < n; j++) new_nums.push_back(nums[j]);
-            genrateCombinations(new_nums, temp, result);
-            temp.pop_back();
+            swap(nums[c], nums[i]);
+            genrateCombinations(nums, c + 1, result);
+            swap(nums[c], nums[i]);
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         set<vector<int>> setResult;
-        vector<int> temp;
-        genrateCombinations(nums, temp, setResult);
+        genrateCombinations(nums, 0, setResult);
         vector<vector<int>> result (setResult.begin(), setResult.end());
         return result;
     }
