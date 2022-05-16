32425b31fec96561b63af635858cce70f15405b9
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-16 22:57:38 +0530

    Added solution - LeetHub

---
+++ Longest subarray with sum divisible by K - GFG/longest-subarray-with-sum-divisible-by-k.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int longSubarrWthSumDivByK(int arr[], int n, int k)
+	{
+	    unordered_map<int, int> mp;
+	    mp[0] = -1;
+	    int size = 0, sum = 0;
+	    for(int i = 0; i < n; i++)
+	    {
+	        sum += arr[i];
+	        int rem = sum % k;
+	        if(rem < 0)
+	        {
+	            rem += k;
+	        }
+	        if(mp.find(rem) == mp.end())
+	        {
+	            mp[rem] = i;
+	        }
+	        else
+	        {
+	            size = max(size, i - mp[rem]);
+	        }
+	    }
+	    return size;
+	}
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n,k,i;
+	cin>>n>>k; int arr[n];
+	for(i=0;i<n;i++)
+	cin>>arr[i];
+	Solution ob;
+	cout<<ob.longSubarrWthSumDivByK(arr, n, k)<<"\n";
+	}
+	return 0;	 
+}
+  // } Driver Code Ends
