bf1fd35fb0ecfd9e6737069bc543751e11413ebd
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-24 01:22:39 +0530

    Time: 672 ms (18.61%), Space: 35.8 MB (47.01%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -6,10 +6,11 @@ public:
         
        return solve(0,m,n,strs.size(),strs);
     }
-    int solve(int start,int m,int n,int size,vector<string>&strs){
-        if(start>=size || m<0 || n<0)
+    int solve(int start,int m,int n,int size,vector<string>&strs)
+    {
+        if(start >= size || m < 0 || n < 0)
             return 0;
-        if(m==0 && n==0)
+        if(m == 0 && n == 0)
             return 0;
         if(dp[start][m][n]!=-1)
             return dp[start][m][n];
@@ -17,8 +18,9 @@ public:
         int i=start;
         int zeroes=count(strs[i].begin(),strs[i].end(),'0');
         int ones=strs[i].size()-zeroes;
+        
         if(m>=zeroes && n>=ones)
-            return dp[start][m][n]= max(1+ solve(i+1, m-zeroes, n - ones,size,strs),solve(i+1,m,n,size,strs));
+            return dp[start][m][n] = max(1+ solve(i+1, m-zeroes, n - ones,size,strs),  solve(i+1,m,n,size,strs));
         else
             return dp[start][m][n]=solve(i+1,m,n,size,strs);
     }
