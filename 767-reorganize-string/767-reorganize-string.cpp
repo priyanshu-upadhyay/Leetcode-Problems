652de8a8cf5da91ad0fbda92871876a841a0aa8d
Priyanshu Upadhyay <61390310+priyanshu-upadhyay@users.noreply.github.com>
2022-05-31 14:06:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (66.84%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        
+        vector<int> freq(26, 0);
+        int n = s.length();
+        int maxFreq = 0;
+        char maxFreqChar = 'a';
+        for(int i = 0; i < n; i++)
+        {
+            freq[s[i] - 'a']++;
+            int currFreq = freq[s[i] - 'a'];
+            if(maxFreq < currFreq)
+            {
+                maxFreq = currFreq;
+                maxFreqChar = s[i];
+            }
+        }
+        
+        // cout<<maxFreq<<"\n";
+        if(maxFreq > (n+1)/2 ) 
+        {
+            return "";
+        }
+        
+        string result;
+        result.append(n, '.');
+        int i = 0;
+        
+        while(freq[maxFreqChar - 'a'] > 0)
+        {
+            result[i] = maxFreqChar;
+            i = i + 2;
+            freq[maxFreqChar - 'a']--;
+        }
+        
+        for(int j = 0; j < n; j++)
+        {
+            
+            
+            while(freq[s[j] - 'a'] > 0)
+            {
+                if(i >= n) i = 1;
+                result[i] = s[j];
+                freq[s[j] - 'a']--;
+                i = i + 2;
+            }
+        }
+        return result;
+        
+        
+    }
+};
